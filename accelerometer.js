196d2b1d9a8e768bcaf82982038c9bf8a07598b2
Marius Gundersen <me@mariusgundersen.net>
2017-09-04 22:51:26 +0200

    trying gpio instead

--- accelerometer.js
+++ accelerometer.js
@@ -1,10 +1,10 @@
-const GpioPin = require('onoff').Gpio;
+const GpioPin = require('gpio-promise');
 const i2c = require('./i2c.js');
 
-const interrupt = new GpioPin(71, 'in', 'rising'); //pin 11
+const interrupt = new GpioPin(71); //pin 11
 
 i2c.initialize().then(async () => {
-  interrupt.watch((err, value) => {
-    console.write(err, value);
-  })
+  interrupt.on('change', function(value) {
+    console.log('change', value);
+ });
 }).catch(r => console.error(r));
