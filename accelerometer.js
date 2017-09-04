5b8e129e6050c9ca60796a3c2999c4108810f743
Marius Gundersen <me@mariusgundersen.net>
2017-09-04 23:12:41 +0200

    async

--- accelerometer.js
+++ accelerometer.js
@@ -6,7 +6,7 @@ const interrupt = new GpioPin(71); //pin 11
 i2c.initialize().then(async () => {
   await i2c.getInterrupt();
   await interrupt.in();
-  interrupt.on('change', function(value) {
+  interrupt.on('change', async function(value) {
     console.log('change', value, await i2c.getInterrupt());
  });
 }).catch(r => console.error(r));
