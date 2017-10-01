10c77da02454ec8a1fc58007d30f881fa92539d3
Marius Gundersen <me@mariusgundersen.net>
2017-10-01 23:07:47 +0200

    export pin on startup

--- accelerometer.js
+++ accelerometer.js
@@ -2,6 +2,7 @@ const GpioPin = require('gpio-promise');
 const i2c = require('./i2c.js');
 
 const interrupt = new GpioPin(71); //pin 11
+interrupt.export(e => console.log(e));
 
 exports.start = function start(onMotion){
   i2c.initialize().then(async () => {
@@ -14,9 +15,9 @@ exports.start = function start(onMotion){
         await onMotion().catch(e => console.warn(e));
         console.log('change', value, (await i2c.getInterrupt()).toString(16));
       }
-   });
-   const r = await i2c.getInterrupt();
-   console.log('on rising edge', r);
+    });
+    const r = await i2c.getInterrupt();
+    console.log('on rising edge', r);
   }).catch(r => console.error(r));
 }
 
