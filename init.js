ee10ec83e1fa6f327c44019f20bad17e6acb485d
Marius Gundersen <me@mariusgundersen.net>
2017-10-01 12:33:49 +0200

    Made init.js work

--- init.js
+++ init.js
@@ -1,8 +1,18 @@
-var hue = require("node-hue-api"),
-timeout = 2000; // 2 seconds
+const hue = require("node-hue-api");
+const prompt = require('prompt-promise');
+const fs = require('fs');
 
 var displayBridges = function(bridge) {
 console.log("Hue Bridges Found: " + JSON.stringify(bridge));
 };
-
-hue.upnpSearch(timeout).then(displayBridges).done();
+(async () => {
+  const bridges = await hue.upnpSearch(2000);
+  console.log(bridges);
+  const bridge = bridges[0];
+  const api = new hue.HueApi();
+  await prompt('Go press the button, then hit enter');
+  const username = await api.createUser(bridge.ipaddress);
+  console.log('username:', username);
+  fs.writeFileSync('.philips-hue.json', JSON.stringify({username, bridge: bridge.ipaddress}));
+  console.log('done');
+})();
